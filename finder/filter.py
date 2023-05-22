19556be931d27c09bdaee1881ef79cf3196e822d
Denis Kraev <deniskraevcc@gmail.com>
2023-05-22 16:26:29 +0300

    added filter api

--- finder/filter.py
+++ finder/filter.py
@@ -1,47 +1,49 @@
 import json
 import os
-
-json_files = ['.//data/hotels.json']
-objects = []
-
-for file_name in json_files:
-    with open(file_name, 'r', encoding='utf-8') as f:
-        objects += json.load(f)
+from flask import request, jsonify, Blueprint
 
 
-def filter_objects_by_criteria(object_list, object_criteria):
-    object_filtered = []
-    for obj in object_list:
-        if obj.get('dictionary_data', {}).get('city') == object_criteria.get('city') and \
-                obj.get('dictionary_data', {}).get('stars') == object_criteria.get('stars'):
-            object_filtered.append(obj)
-    return object_filtered
 
 
-criteria = {'city': '5bbe8a8a9e7fcb000f78a92d', 'stars': '3'}
-filtered_objects = filter_objects_by_criteria(objects, criteria)
+@index_bp.route('/data', methods=['POST'])
+def post_data():
+    city = request.json['city']
+    stars = request.json['stars']
 
+    json_files = ['.//data/hotels.json']
+    objects = []
 
-def calculate_ratings(object_list):
-    object_ratings = {}
-    for obj in object_list:
-        obj_rating = obj.get('dictionary_data', {}).get('stars', 0)
+    for file_name in json_files:
+        with open(file_name, 'r', encoding='utf-8') as f:
+            objects += json.load(f)
 
-        if obj['dictionary_data']['title'] not in object_ratings:
-            object_ratings[obj['dictionary_data']['title']] = obj_rating
-    return object_ratings
+    def filter_objects_by_criteria(object_list, object_criteria):
+        object_filtered = []
+        for obj in object_list:
+            if obj.get('dictionary_data', {}).get('city') == object_criteria.get('city') and \
+                    obj.get('dictionary_data', {}).get('stars') == object_criteria.get('stars'):
+                object_filtered.append(obj)
+        return object_filtered
 
+    criteria = {'city': city, 'stars': stars}
+    filtered_objects = filter_objects_by_criteria(objects, criteria)
 
-ratings = calculate_ratings(filtered_objects)
+    def calculate_ratings(object_list):
+        object_ratings = {}
+        for obj in object_list:
+            obj_rating = obj.get('dictionary_data', {}).get('stars', 0)
 
+            if obj['dictionary_data']['title'] not in object_ratings:
+                object_ratings[obj['dictionary_data']['title']] = obj_rating
+        return object_ratings
 
-def get_sorted_objects(object_list, object_ratings, n):
-    sorted_objects = sorted(object_list, key=lambda obj: object_ratings.get(obj['dictionary_data']['title'], 0),
-                            reverse=True)
-    return sorted_objects[:n]
+    ratings = calculate_ratings(filtered_objects)
 
+    def get_sorted_objects(object_list, object_ratings, n):
+        sorted_objects = sorted(object_list, key=lambda obj: object_ratings.get(obj['dictionary_data']['title'], 0),
+                                reverse=True)
+        return sorted_objects[:n]
 
-recommendations = get_sorted_objects(filtered_objects, ratings, 150)
+    recommendations = get_sorted_objects(filtered_objects, ratings, 150)
 
-##print(recommendations)
-print(f"Number of recommendations: {len(recommendations)}")
+    return jsonify(recommendations), 200
--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ from flask_pymongo import PyMongo
 from config import SECRET_KEY, MONGO_URI
 from flask import Blueprint, request, jsonify
 from models.user import User
+import json
 
 
 app = Flask(__name__)
@@ -39,14 +40,46 @@ def get_data():
 
 @index_bp.route('/data', methods=['POST'])
 def post_data():
-    name = request.json['name']
-    value = request.json['value']
+    city = request.json['city']
+    stars = request.json['stars']
 
-    if name and value:
-        mongo.db.data.insert_one({'name': name, 'value': value})
-        return jsonify({'message': 'Data saved.'}), 200
-    else:
-        return jsonify({'error': 'Invalid input.'}), 400
+    json_files = ['.//data/hotels.json']
+    objects = []
+
+    for file_name in json_files:
+        with open(file_name, 'r', encoding='utf-8') as f:
+            objects += json.load(f)
+
+    def filter_objects_by_criteria(object_list, object_criteria):
+        object_filtered = []
+        for obj in object_list:
+            if obj.get('dictionary_data', {}).get('city') == object_criteria.get('city') and \
+                    obj.get('dictionary_data', {}).get('stars') == object_criteria.get('stars'):
+                object_filtered.append(obj)
+        return object_filtered
+
+    criteria = {'city': city, 'stars': stars}
+    filtered_objects = filter_objects_by_criteria(objects, criteria)
+
+    def calculate_ratings(object_list):
+        object_ratings = {}
+        for obj in object_list:
+            obj_rating = obj.get('dictionary_data', {}).get('stars', 0)
+
+            if obj['dictionary_data']['title'] not in object_ratings:
+                object_ratings[obj['dictionary_data']['title']] = obj_rating
+        return object_ratings
+
+    ratings = calculate_ratings(filtered_objects)
+
+    def get_sorted_objects(object_list, object_ratings, n):
+        sorted_objects = sorted(object_list, key=lambda obj: object_ratings.get(obj['dictionary_data']['title'], 0),
+                                reverse=True)
+        return sorted_objects[:n]
+
+    recommendations = get_sorted_objects(filtered_objects, ratings, 5)
+
+    return jsonify(recommendations), 200
 
 
 app.config["MONGO_URI"] = MONGO_URI
