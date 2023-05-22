7939bff6b4e0158c22302d8868d56f23ab543431
Nick White <bbycinka@yandex.ru>
2023-05-22 21:03:59 +0300

    feat:(frontend): css modules

--- my-app/src/pages/MainPage/MainPage.jsx
+++ my-app/src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { testThunk } from "../../service/slice/testSlice";
 import { useEffect } from "react";
-
+import styleMain from './MainPage.module.css'
 export const MainPage = () => {
   useEffect(() => {
     dispatch(testThunk());
@@ -10,7 +10,8 @@ export const MainPage = () => {
   const dispatch = useDispatch();
   return (
     <div>
-      <h1>Main Page</h1>
+      <h1 className={styleMain.title}>Main Page</h1>
+      <h1 className={styleMain.blue}>color blue Page</h1>
       {users &&
         users.map((user) => {
           return <li key={user.id}>{user.name}</li>;
