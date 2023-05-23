4003d53a19cb1acf5c802173eab526036608e104
jTroi <ctepanr0manov@yandex.ru>
2023-05-23 07:23:19 +0300

    header components

--- my-app/src/pages/MainPage/MainPage.jsx
+++ my-app/src/pages/MainPage/MainPage.jsx
@@ -2,6 +2,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { testThunk } from "../../service/slice/testSlice";
 import { useEffect } from "react";
 import styleMain from './MainPage.module.css'
+import { Header } from "../../components/Header/HeaderLine";
+
+
 export const MainPage = () => {
   useEffect(() => {
     dispatch(testThunk());
@@ -9,7 +12,8 @@ export const MainPage = () => {
   const { users } = useSelector((state) => state.test);
   const dispatch = useDispatch();
   return (
-    <div>
+    <div className={styleMain.container}>
+      <Header />
       <h1 className={styleMain.title}>Main Page</h1>
       <h1 className={styleMain.blue}>color blue Page</h1>
       {users &&
